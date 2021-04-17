216fe487ec13a72c596e72f8457d56acfd566ce0
Mateusz Godlewski <56505508+Mateusz-Godlewski@users.noreply.github.com>
2021-04-17 12:09:41 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 
@@ -12,9 +12,24 @@ def home():
     return render_template("index.html", response=response)
 
 
-@app.route("/<name>")
-def page(name):
-    return render_template(name)
+# @app.route("/success.html", methods=["POST"])
+# def success():
+#     return render_template("success.html")
+
+
+@app.route("/contact.html", methods=["GET", "POST"])
+def contact_page():
+    if request.method == "POST":
+        heading = "Successfully sent your message!"
+        username = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        message = request.form["message"]
+        print(f"{username}, {email}, {phone}, {message}")
+        return render_template("contact.html", username=username, email=email, phone=phone, message=message, heading=heading)
+    else:
+        heading = "Contact Me"
+        return render_template("contact.html", heading=heading)
 
 
 @app.route("/post.html/<num>")
@@ -22,6 +37,11 @@ def post(num):
     response = requests.get(url="https://api.npoint.io/0067e63917ca7a5034d9").json()
     blog_post = response[int(num) - 1]
     return render_template("post.html", num=num, response=blog_post)
+
+
+@app.route("/<name>")
+def page(name):
+    return render_template(name)
 
 
 if __name__ == "__main__":
